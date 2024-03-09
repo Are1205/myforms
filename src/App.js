aea2854b63d6d0a623bbbac61a878d57b0a153bc
Andres Arevalo Fajardo <a.arevalof@uniandes.edu.co>
2024-03-08 19:14:44 -0500

    verificaciones asincronicas

--- src/App.js
+++ src/App.js
@@ -9,30 +9,28 @@ function App() {
   const handleEmailChange = ((e) => {
     setFormValues({...formValues, email: e.target.value})
   });
- 
+
   const handlePasswordChange = ((e) => {
     setFormValues({...formValues, password: e.target.value})
   });
- 
+
   const handleSelectChange = ((e) => {
     setFormValues({...formValues, favClass: e.target.value})
   });
 
-  // const validationStates = ((e) =>{
-  //   emailState: formValues.email.includes('@'), 
-  //   passwordState: formValues.password.includes(/[a-zA-Z]/) && formValues.password.includes(/[0-9]/) && formValues.password.length >= 9
-  // }
-
   const clickSubmit = (() => {
-    const emailState= formValues.email.includes('@')
-    console.log(emailState)
-    setValidationStates ({...validationStates, emailState: emailState})
-    const regex = /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{9,}$/;
-    const passwordState= regex.test(formValues.password);
-    console.log(passwordState)
-    setValidationStates ({...validationStates, passwordState: passwordState})
-    
-    if (emailState && passwordState){
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/;
+    const emailStateNew = formValues.email.includes('@');
+    const passwordStateNew = regex.test(formValues.password);
+
+    setValidationStates({
+      ...validationStates,
+      emailState: emailStateNew,
+      passwordState: passwordStateNew
+    });
+
+    if (validationStates.passwordState && validationStates.emailState){
+      console.log(validationStates)
       //Call fetch
       alert(JSON.stringify(formValues))
     }
@@ -43,18 +41,17 @@ function App() {
   return (
     <div>
       <h1>Ejemplo de formularios!</h1>
-     
       <Form>
       <Form.Group className="mb-6 " controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={formValues.email}/>
-        { !validationStates.emailState && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
+        <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={formValues.email} className={validationStates.emailState ? 'input-valid' : 'input-invalid'}/>
+        {!validationStates.emailState && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
       </Form.Group>
- 
+
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={formValues.password} />
-        { !validationStates.passwordState && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
+        <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={formValues.password} className= {validationStates.passwordState ? 'input-valid' : 'input-invalid'}/>
+        {!validationStates.passwordState && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Label>Favorite Class</Form.Label>
